9c412089153dd048f3049c1608ed6027ea999e0e
matthew-kal <matthewkalender@gmail.com>
2025-08-26 10:33:53 -0400

    small additions

--- src/components/cards/apps/data/Projects.tsx
+++ src/components/cards/apps/data/Projects.tsx
@@ -2,6 +2,17 @@ import WorkItemType from "./app";
 
 export const Projects: WorkItemType[] = [
   {
+    title: "RAGgedy",
+    description:
+      "A local-first desktop app built for querying massive documents. Leverages RAG and SLMs to deliver answers to any question.",
+    icon: "/works/vector.png",
+    image: "/works/vector.png",
+    path: "https://github.com/matthew-kal/RAGgedy",
+    tags: ["Electron.js", "Node.js", "LanceDB", 
+            "Python", "SLMs"],
+    hasBorder: true, 
+  }, 
+  {
     title: "SurgiCalm",
     description:
       "Mobile application aimed at reducing stress in pre- and post-operative patients. The platform delivers evidence-based practices such as guided breathing, yoga routines, nutrition planning, and mindfulness techniques to support holistic recovery and improve patient well-being.",
@@ -11,6 +22,16 @@ export const Projects: WorkItemType[] = [
     tags: ["React Native", "React.js", "Django", 
             "MySQL", "AWS S3", "AWS RDS", 
             "AWS EC2", "Expo" ],
+    hasBorder: true,
+  },
+  {
+    title: "C++ Webserver",
+    description:
+      "Performant HTTP 1.1 server built entirely in C/C++. Though this project has no real use, I'm using it as an opportunity to get familar with networks on a granular scale.",
+    icon: "/works/websererver.png",
+    image: "/works/webserver.png",
+    path: "https://github.com/matthew-kal/CPP-Networking",
+    tags: ["C++", "C" ],
     hasBorder: true,
   },
   {
